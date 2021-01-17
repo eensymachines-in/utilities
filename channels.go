83b649223d6860e637d6547cf6a61abf47d5bcd3
kneerunjun <kneerunjun@gmail.com>
2021-01-17 09:55:54 +0530

    channels safe close, more comments

--- channels.go
+++ channels.go
@@ -3,6 +3,9 @@ package utilities
 import "sync"
 
 // SafeCloseChn : channels need to be closed once, this can help to do that not more than once
+// closure here deals with closing a channnel safely once using a niladic function
+// when a channel has multiple points from where its closed,
+// these multiple points cannot be predicted as in sequence
 func SafeCloseChn(ch chan interface{}) func() {
 	var once sync.Once
 	return func() {
