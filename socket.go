e8835783b44f49f8001dd1045fb5810a3ba444e7
kneerunjun <kneerunjun@gmail.com>
2021-01-21 14:22:46 +0530

    ListenOnUnixSocket needed retry mechanism before failure

--- socket.go
+++ socket.go
@@ -3,15 +3,25 @@ package utilities
 import (
 	"net"
 	"os"
+
 	log "github.com/sirupsen/logrus"
 )
 
 // ListenOnUnixSocket : sets up a listener, returns a function to start, stop and error incase the socket could not be listened
 // https://eli.thegreenplace.net/2019/unix-domain-sockets-in-go/
 func ListenOnUnixSocket(sock string, handler func(net.Conn)) (func(), func(), error) {
-	l, err := net.Listen("unix", sock)
-	if err != nil {
-		return nil, nil, err
+	retries := 2
+	var l net.Listener
+	var err error
+	for i := retries; i > 0; i-- {
+		l, err = net.Listen("unix", sock)
+		if err != nil {
+			if i == 1 {
+				return nil, nil, err
+			}
+			continue
+		}
+		break
 	}
 	start := func() {
 		for {
@@ -24,8 +34,8 @@ func ListenOnUnixSocket(sock string, handler func(net.Conn)) (func(), func(), er
 	}
 	stop := func() {
 		l.Close()
-		err :=os.RemoveAll(sock) // for the next run we need the file to be removed .. this is part of server cleanup
-		if err !=nil{
+		err := os.RemoveAll(sock) // for the next run we need the file to be removed .. this is part of server cleanup
+		if err != nil {
 			log.Errorf("Failed to clear socket %s", err)
 		}
 	}
