c8368c1878419cad4e7c4486ad999791f51f5cab
Niranjan <kneerunjun@gmail.com>
2024-05-06 12:43:50 +0530

    function CORS with restrictive origin

--- gin.go
+++ gin.go
@@ -6,11 +6,33 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// CORS : this allows all cross origin requests
+/* restrictive  origin policy, you can set only on domain, with port or aubdomain that you want to restrict the requests from */
+func CorsWithOrigin(origin string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		// First, we add the headers with need to enable CORS
+		// Make sure to adjust these headers to your needs
+		c.Header("Access-Control-Allow-Origin", origin)
+		c.Header("Access-Control-Allow-Methods", "*")
+		c.Header("Access-Control-Allow-Headers", "*")
+		c.Header("Content-Type", "application/json")
+		// Second, we handle the OPTIONS problem
+		if c.Request.Method != "OPTIONS" {
+			c.Next()
+		} else {
+			// Everytime we receive an OPTIONS request,
+			// we just return an HTTP 200 Status Code
+			// Like this, Angular can now do the real
+			// request using any other method than OPTIONS
+			c.AbortWithStatus(http.StatusOK)
+		}
+	}
+}
+
+/* Wide open cors policy, no restrictions on the origin, but all origins have to be working on port 80 */
 func CORS(c *gin.Context) {
 	// First, we add the headers with need to enable CORS
 	// Make sure to adjust these headers to your needs
-	c.Header("Access-Control-Allow-Origin", "http://aqua.eensymachines.in, http://localhost:8080")
+	c.Header("Access-Control-Allow-Origin", "*")
 	c.Header("Access-Control-Allow-Methods", "*")
 	c.Header("Access-Control-Allow-Headers", "*")
 	c.Header("Content-Type", "application/json")
@@ -25,3 +47,5 @@ func CORS(c *gin.Context) {
 		c.AbortWithStatus(http.StatusOK)
 	}
 }
+
+// CORS : this allows all cross origin requests
